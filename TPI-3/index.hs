e2fb3ecba1e69a76b865e4c9801aa6ba2e5e6477
Emanuel <cascoemanuel@gmail.com>
2017-05-05 01:58:45 -0300

    changes in TPI-3

--- TPI-3/index.hs
+++ TPI-3/index.hs
@@ -3,38 +3,27 @@ data Aventurero = UnAventurero {
   recorrido :: [(Int, Int)]
 } deriving Show
 
+type Vector = (Int, Int)
+
 juan = UnAventurero "Juancho" [(0,0), (5,0), (7,0), (7,4), (11,2), (11,(-5))]
 manu = UnAventurero "Emanuel" [(0,0), (10,0)]
 john = UnAventurero "Lennon" [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0)]
 
 ---
 
-delete :: Eq a => a -> [a] -> [a]
-delete deleted [] = []
-delete deleted (x:xs)
-  | (not.elem deleted) (x:xs) = (x:xs)
-  | x == deleted = xs
-  | otherwise = x : delete deleted xs
+zonaMaldita :: (Vector,Vector)
+zonaMaldita = ((10,0),(16,6))
 
-intersect :: Eq a => [a] -> [a] -> [a]
-intersect [] _ = []
-intersect _ [] = []
-intersect (x:xs) ys
-  | x `elem` ys = x : intersect xs (delete x ys)
-  | otherwise = intersect xs ys
+between :: Ord a => a -> a -> a -> Bool
+between xi xf x = xi <= x && x <= xf
 
-createZone :: ((Int, Int),(Int, Int)) -> [(Int, Int)]
-createZone ((xi,yi), (xf,yf)) = [ (x,y) | x <- [xi..xf], y <- [yi..yf] ]
+pasoPorZona :: (Vector,Vector) -> Vector -> Bool
+pasoPorZona ((xi,yi),(xf,yf)) (x, y) =  between (xi,yi) (xf,yf) (x, y)
 
 ---
-
-zonaMaldita :: [(Int, Int)]
-zonaMaldita = createZone ((10,0), (16,6))
 
 estaMaldito :: Aventurero -> Bool
-estaMaldito = (not.null) . intersect zonaMaldita . recorrido
-
----
+estaMaldito =  any (pasoPorZona zonaMaldita) . recorrido
 
 nombresMalditos :: [Aventurero] -> [String]
 nombresMalditos = map nombre . filter estaMaldito
